dcb8d9fb629fe2f847aae302a111f05b42fa1f4d
Sivaprakash <sivpraka@cisco.com>
2019-02-18 23:14:11 +0530

    Create arrays.rb
    Arrays Example

---
+++ arrays.rb
@@ -0,0 +1,22 @@
+system "clear"
+
+number = [1,2,3,4,5]
+
+names=["Alex","Bob","Charlie","Don",number]
+
+puts names
+
+puts number.length
+
+puts names[2]
+
+puts names[4]
+
+puts "Array Values:"
+
+puts names[4][2]
+
+puts "Array: #{names[4][2]}"
+
+name = "Ada"
+puts "Hello, #{name}!"
